fb7135c83e70a22946b7a709c6282470305508bc
Karine.J <karine.jamet203@gmail.com>
2016-01-08 11:36:12 +0100

    debut de la fonction playAlone

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $('.reload').hide();
   // creation classe du jeux plus  ou moins
+  var maxtmp=Math.floor(Math.random() * 1000)+1000;
 
   var Game = function(max) {
     // Definition de la propirité answer meme si Max absent
+    this.max = max;
     this.answer = max ? Math.floor(Math.random() * max) : Math.floor(Math.random() * 10000);
     this.tantative = 0;
     // Definition de la methode test qui dit si le joueur a bien deviner
@@ -27,7 +29,10 @@ $(document).ready(function() {
 
 // Fonctionalité de la page
 
-  var play = new Game(2000);
+  var play = new Game(maxtmp);
+$('input[type="range"]').prop('max', maxtmp);
+  console.log(play.max);
+  console.log(play.answer);
 
 
   $('input[name="go"]').on("click", function(e) {
@@ -51,16 +56,19 @@ $(document).ready(function() {
 
   $('input[name="reload"]').on("click", function(e) {
     e.preventDefault();
-    play = new Game(2000);
+    var play = new Game(maxtmp);
+    $('input[type="range"]').prop('max', maxtmp);
     $('.guess').show();
     $('.reload').hide();
   });
 
   $('input[name="playAlone"]').on("click", function(e) {
     e.preventDefault();
-    play = new Game(2000);
-    $('.guess').show();
-    $('.reload').hide();
+    var essay =Math.floor(Math.random() * play.max);
+    $('input[type="range"]').val(essay);
+    $('input[type="number"]').val(essay);
+
+
   });
 
 
